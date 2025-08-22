29327ec7f159a1c276838ba1832f40dd21f1e8f9
PelleSwelle <peterdonvang@protonmail.com>
2025-08-22 23:40:25 +0200

    changed headline

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import { ModeToggle } from "./themeToggle";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to My personal portfolio!</h1>
+      <h1 className="text-4xl font-bold">My Name is Peter Dønvang and I enjoy building things!</h1>
       <ModeToggle/>
       <CarouselDemo/>
+
+      {/* insert links to linkedin, github and so on */}
     </main>
   );
 }
