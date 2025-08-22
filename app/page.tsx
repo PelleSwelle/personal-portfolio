263bca426bc5e111ef1ff54383d180772ffb0039
PelleSwelle <peterdonvang@protonmail.com>
2025-08-22 22:58:53 +0200

    added first batch of content

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
+import { CarouselDemo } from "./carouselDemo";
+import { ModeToggle } from "./themeToggle";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1>
-      <p className="mt-4 text-lg">This is a simple example of a Next.js application.</p>
-      <Image
-        src="/nextjs-logo.png"
-        alt="Next.js Logo"
-        width={200}
-        height={200}
-        className="mt-8"
-      />
+      <h1 className="text-4xl font-bold">Welcome to My personal portfolio!</h1>
+      <ModeToggle/>
+      <CarouselDemo/>
     </main>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  Images: {
+    remotePatterns: [
+      new URL("https://via.placeholder.com"),
+    ],
+  }
 };
 
 export default nextConfig;
