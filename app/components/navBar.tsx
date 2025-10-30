7c2e2d2d99272703174f46e2bfd8357eb0ab29c5
PelleSwelle <peterdonvang@protonmail.com>
2025-10-30 17:19:59 +0100

    proper switching

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import { useState } from "react"
 import Link from "next/link"
 import { Menu } from "@deemlol/next-icons"
 import {
@@ -13,9 +14,41 @@ import {
 import { useMediaQuery } from 'react-responsive'
 
 const BurgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div className="relative flex max-w-max flex-1 justify-start ml-4 mt-4">
+    <div className="relative flex max-w-max flex-1 justify-start ml-4 mt-4" onClick={() => setIsOpen(!isOpen)}>
       <Menu />
+      {isOpen ?
+        <NavigationMenu viewport={false} className="fixed">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link href="/">Home</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link href="/projects">Projects</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link href="/about">About</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                <Link href="/contact">Contact</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        : null
+      }
     </div>
   )
 }
@@ -55,6 +88,8 @@ const NavMenu = () => {
 }
 
 export function NavigationMenuDemo() {
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false)
+
   const isDesktopOrLaptop = useMediaQuery({
     query: '(min-width: 1224px)'
   })
